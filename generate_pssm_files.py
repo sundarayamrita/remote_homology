ac7d93690632109b4e03f49def745a59031e2716
sundarayamrita <sundarayamrita@gmail.com>
2020-12-03 16:54:10 +0530

    added pssm generating script

---
+++ generate_pssm_files.py
@@ -0,0 +1,34 @@
+import os
+import argparse
+from pathlib import Path
+import subprocess
+
+query_dir = Path.cwd() / "Strain_plus"
+pssm_dir = Path.cwd() / "PSSMs"
+homologue_dir = Path.cwd() / "Homologues"
+
+#if not os.path.exists(pssm_dir):
+if not pssm_dir.is_dir():
+	os.mkdir(pssm_dir)
+#if not os.path.exists(homologue_dir):
+if not homologue_dir.is_dir():
+	os.mkdir(homologue_dir)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-db_path", help = "path to database such as pdb", type = str)
+
+args = parser.parse_args()
+if args.db_path:
+	database = args.db_path
+#print(database)
+#print(Path.cwd())
+for query_file in query_dir.iterdir():
+
+	filename = query_file.stem
+	out_homologues_file = os.path.join(str(homologue_dir), filename + "_out_homologues.txt")
+	num_iters = "3"
+	pssm_output = os.path.join(str(pssm_dir), filename + "_pssm.txt")
+	blast_cmd = "psiblast -query " + str(query_file) + " -db " + database + " -out " + out_homologues_file + " -num_iterations " + num_iters + " -out_ascii_pssm " + pssm_output
+	os.system(blast_cmd)
+
+
