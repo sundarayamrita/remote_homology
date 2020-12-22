a47f9cdb00d5809a4bcee4b3b4a22857b2d1ec10
sundarayamrita <sundarayamrita@gmail.com>
2020-12-22 12:46:13 +0530

    vectorised argmax

--- pseudo_protein.py
+++ pseudo_protein.py
@@ -4,10 +4,9 @@ import psuedo_protein_generation
 aa = ['A','R', 'N', 'D', 'C', 'Q','E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T','W', 'Y', 'V']
 
 pssm = np.asarray(psuedo_protein_generation.pssm_extraction())
-indices = []
-for mul_ali in pssm:
-	indices.append(np.argmax(mul_ali, axis = 0))
-indices = np.asarray(indices)
+#for mul_ali in pssm:
+#	indices.append(np.argmax(mul_ali, axis = 0))
+indices = np.argmax(pssm, axis = 1) #np.asarray(indices)
 pseudo_protein = []
 for idx in indices:
 	pseudo_protein.append(aa[idx])
