274d512c8b5c4d34c7e516036c09f42f9c66bb02
Srinidhi Kulkarni <srinidhi2810@gmail.com>
2020-12-24 14:56:19 +0530

    Extract Psssm

--- Extract_PsSM.py
+++ Extract_PsSM.py
@@ -1,35 +1,33 @@
 import csv
-
 import argparse
 from pathlib import Path
 import subprocess
 
 def pssm_extraction(filename):
+    #filename=Path(filename)
     inputmat=[]
     with open(filename,'r') as f:
         lines = f.readlines()[1:]
         linesnum=len(lines)
         
         del lines[0]
-        del lines[linesnum-7:]
         
-       
         for line in lines:
             elements = line.strip().split()
             temp=[]
             for i in elements:
                 if(i!='' and i<'A'):
                     temp.append(i)
-            if(len(temp[1:21])>0):
+            if(len(temp[1:21])>1):
                 inputmat.append(temp[1:21])
                         
-               
-    return(inputmat)
+
+    return(inputmat)           
+    
 
 
 
-
-
+#pssm_extraction(r"C:\Users\meera\OneDrive\Desktop\Homology\blast_gen_files_single\Remote-homology\positive_train_pssm_new\query_1037_pssm.txt")
 
 
 
