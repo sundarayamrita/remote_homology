a353a057887c1a594d4794155dc84a346dfda8b8
Srinidhi Kulkarni <srinidhi2810@gmail.com>
2020-12-24 14:08:31 +0530

    Extraction of PSSM

--- Extract_PsSM.py
+++ Extract_PsSM.py
@@ -5,16 +5,15 @@ from pathlib import Path
 import subprocess
 
 def pssm_extraction(filename):
-    
     inputmat=[]
     with open(filename,'r') as f:
         lines = f.readlines()[1:]
         linesnum=len(lines)
         
         del lines[0]
-        del lines[linesnum-15:]
+        del lines[linesnum-7:]
         
-
+       
         for line in lines:
             elements = line.strip().split()
             temp=[]
@@ -23,9 +22,17 @@ def pssm_extraction(filename):
                     temp.append(i)
             if(len(temp[1:21])>0):
                 inputmat.append(temp[1:21])
-        
-        
-    
+                        
+               
     return(inputmat)
+
+
+
+
+
+
+
+
+
 
 
