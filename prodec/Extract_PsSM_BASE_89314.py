b6ee534306b6290bc9138bb73d99c8c5e2c980d0
sundarayamrita <sundarayamrita@gmail.com>
2021-01-04 19:31:54 +0530

    README's back

---
+++ prodec/Extract_PsSM_BASE_89314.py
@@ -0,0 +1,36 @@
+import csv
+import argparse
+from pathlib import Path
+import subprocess
+
+def pssm_extraction(filename):
+    #filename=Path(filename)
+    inputmat=[]
+    with open(filename,'r') as f:
+        lines = f.readlines()[1:]
+        linesnum=len(lines)
+        
+        del lines[0]
+        
+        for line in lines:
+            elements = line.strip().split()
+            temp=[]
+            for i in elements:
+                if(i!='' and i<'A'):
+                    temp.append(i)
+            if(len(temp[1:21])>1):
+                inputmat.append(temp[1:21])
+                        
+
+    return(inputmat)           
+    
+
+
+
+#pssm_extraction(r"C:\Users\meera\OneDrive\Desktop\Homology\blast_gen_files_single\Remote-homology\positive_train_pssm_new\query_1037_pssm.txt")
+
+
+
+
+
+
