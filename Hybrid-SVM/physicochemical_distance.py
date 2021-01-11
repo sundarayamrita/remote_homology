7fa5b1160ada5beccdb2e193ead2953d737fa75b
sundarayamrita <sundarayamrita@gmail.com>
2021-01-11 11:17:20 +0530

    vectorisation complete

--- Hybrid-SVM/physicochemical_distance.py
+++ Hybrid-SVM/physicochemical_distance.py
@@ -25,34 +25,39 @@ def get_physico_dist(file_loc, aa_index_path):
 #    print("shape of I", I.shape)
     PDT = np.zeros((num_props, alpha))
     val = 0
-    pdt_l = []
+    pdt = []
 
     for mu in range(1, alpha + 1):
-        for j in range(num_props):
+#        for j in range(num_props):
 # AT TS WE WERE TOLD TO AVOID VARIABLES THAT ARE TO BE USED ONLY IN THE NEXT LINE SUCH AS val
 # AND IF YOU CANNOT AVOID USING IT THEN GIVE IT A GOOD NAME
-            val = 0
-            for i in range(L - mu):
-                val1 = lookup.get(seq[i], 1)
-                val2 = lookup.get(seq[i + mu], 1)
-                val = val + (I[val1 - 1, j] - I[val2 - 1, j]) ** 2
-                val = val / (L - mu)
+#            val = 0
+#            for i in range(L - mu):
+#                val1 = lookup.get(seq[i], 1)
+#                val2 = lookup.get(seq[i + mu], 1)
+#                val = val + (I[val1 - 1, j] - I[val2 - 1, j]) ** 2
+#                val = val / (L - mu)
 
-            PDT[j, mu - 1] = val
+#            PDT[j, mu - 1] = val
         
         p1_idx = np.array([lookup.get(seq[i], 1) - 1 for i in range(L - mu)])
         p2_idx = np.array([lookup.get(seq[i], 1) - 1 for i in range(mu , L)])
-        j = np.pad(np.arange(num_props)[:, np.newaxis], ((0,0),(0,L - mu - 1)), 'edge') #<-- 531x153
-        p1_idx = np.pad(p1_idx[:, np.newaxis], ((0,0),(0, num_props - 1)), 'edge').T #<-- transpose should give right answer
-        p2_idx = np.pad(p2_idx[:, np.newaxis], ((0,0),(0, num_props - 1)), 'edge').T
-        dp = np.sum(np.square(I[p1_idx, j] - I[p2_idx, j]), axis=1) / (L - mu)
-        print("dp shape")
-        print(dp.shape)
-        pdt_l.append(dp)
 
-    print(PDT)
-    print(np.asarray(pdt_l).T)
-    return PDT
+        j = np.pad(np.arange(num_props)[:, np.newaxis], ((0,0),(0,L - mu - 1)), 'edge') #<-- 531x153
+#        print("j shape")
+#        print(j.shape)
+        p1_idx = np.pad(p1_idx[np.newaxis, :], ((0, num_props - 1),(0, 0)), 'edge')
+#        print("p1_idx shape")
+#        print(p1_idx.shape)
+        p2_idx = np.pad(p2_idx[np.newaxis, :], ((0, num_props - 1), (0,0)), 'edge')
+        dp = np.sum(np.square(I[p1_idx, j] - I[p2_idx, j]), axis=1) / (L - mu)
+
+        pdt.append(dp)
+
+#    print(PDT)
+#    print(np.asarray(pdt).T)
+#    return PDT
+    return np.asarray(pdt).T
 
 if __name__ == "__main__":
     file_loc = Path.cwd() / "query_1.txt"
