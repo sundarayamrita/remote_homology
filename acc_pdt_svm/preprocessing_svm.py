90ddf74c78570b47f4b87b2a88961c8950380428
sundarayamrita <sundarayamrita@gmail.com>
2021-01-30 18:17:03 +0530

    appearance formatted and removed os library usage

--- acc_pdt_svm/preprocessing_svm.py
+++ acc_pdt_svm/preprocessing_svm.py
@@ -1,5 +1,5 @@
 from Bio import SeqIO
-import os
+#import os
 import argparse
 from pathlib import Path
 import sys
@@ -7,6 +7,7 @@ sys.path.append("..")
 from prodec.split import spliting
 from prodec.generate_pssm_files import generating_pssm
 from accumulate_dt import distance_transforms as DT
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-seq_file", help = "file containing sequences", type = str)
 parser.add_argument("-db_path", help = "path to database such as pdb", type = str)
@@ -14,8 +15,8 @@ parser.add_argument("-db_path", help = "path to database such as pdb", type = st
 args = parser.parse_args()
 
 indexed_files = Path.cwd()/"indexed_files"
-if not os.path.exists(indexed_files):
-	os.mkdir(indexed_files)
+if not indexed_files.is_dir():
+	Path.mkdir(indexed_files)
 
 sf_index = (Path(args.seq_file).stem)
 start_ind = sf_index.find('.')
@@ -33,10 +34,11 @@ filename = Path(filepath).stem
 
 family_files = Path.cwd()/indexed_files/sf_index
 dataseqs = family_files/filename
-if not os.path.exists(family_files):
-	os.mkdir(family_files)
-if not os.path.exists(dataseqs):
-	os.mkdir(dataseqs)
+
+if not family_files.is_dir():
+	Path.mkdir(family_files)
+if not dataseqs.is_dir():
+	Path.mkdir(dataseqs)
 
 print("The given type of file is:\n", filename)
 
@@ -44,23 +46,23 @@ print("...The splitting into single sequence begins...")
 spliting(filepath, dataseqs)
 print("...The splitting into single sequences ends...")
 print("\n")
+
 pssm_dir = family_files/ (filename + "PSSMs")
 homologue_dir = family_files/(filename + "Homologues")
+
 if not pssm_dir.is_dir() :
 	Path.mkdir(pssm_dir)
 
 if not homologue_dir.is_dir() :
 	Path.mkdir(homologue_dir)
 
-
 superfamily_file = filename + 'pseudo_protein_seq.txt'
 
 print("...The PSSM and Homologues generation begins...\n")
 
-
 generating_pssm(dataseqs, database, pssm_dir, homologue_dir)
-print("\n")
-print("...The PSSM and Homologues generated...\n")
 
-file_loc=family_files/filename
+print("\n...The PSSM and Homologues generated...\n")
+
+file_loc = family_files/filename
 DT(pssm_dir, dataseqs, file_loc)
