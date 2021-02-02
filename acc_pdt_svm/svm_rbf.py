e6272966b3bbf5931498c9ff72dec02010166ab8
Srinidhi Kulkarni <srinidhi2810@gmail.com>
2021-02-02 15:07:05 +0530

    A few changes to be made yet

--- acc_pdt_svm/svm_rbf.py
+++ acc_pdt_svm/svm_rbf.py
@@ -2,19 +2,47 @@ import numpy as np
 import argparse
 from pathlib import Path
 
-pos_x = np.load('pos-train.c.1.1.npy')
-neg_x = np.load('neg-train.c.1.1.npy')
-#print("original x pos 35", pos_x[35])
-#print("original x neg 132", neg_x[132])
+parser = argparse.ArgumentParser()
+parser.add_argument("-data_dir", help = "dataset folder", type = str)
+args = parser.parse_args()
+if not args.data_dir:
+    print("Dataset directory does not exist")
+    exit()
+else:
+    dataset = Path(args.data_dir)
+
+#pos_x = np.load('pos-train.c.1.1.npy')
+#neg_x = np.load('neg-train.c.1.1.npy')
+for file in sorted(dataset.glob('*.npy')):
+    if 'pos-train' in file.name:
+        pos_x = np.load(file)
+    if 'neg-train' in file.name:
+        neg_x = np.load(file)
+    if 'pos-test' in file.name:
+        pos_test_x = np.load(file)
+    if 'neg-test' in file.name:
+        neg_test_x = np.load(file)
 
 pos_y = np.ones((pos_x.shape[0]))
 neg_y = np.zeros((neg_x.shape[0]))
+
+pos_test_y = np.ones((pos_test_x.shape[0]))
+neg_test_y = np.zeros((neg_test_x.shape[0]))
 
 X = np.vstack((pos_x, neg_x))
 print("x shape", X.shape)
 y = np.concatenate((pos_y, neg_y), axis = 0)
 print("y shape", y.shape)
 
+X_test = np.vstack((pos_test_x, neg_test_x))
+print("x test shape", X_test.shape)
+y_test = np.concatenate((pos_test_y, neg_test_y), axis = 0)
+print("y test shape", y_test.shape)
+print("test_postive_samples:",pos_test_y.shape)
+print("test_negative_samples:",neg_test_y.shape)
+
+X=np.vstack((X,X_test))
+y = np.hstack((y.T,y_test.T))
 shuffle_idx = np.random.permutation(X.shape[0])
 X = X[shuffle_idx]
 y = y[shuffle_idx]
@@ -36,15 +64,21 @@ y = y[shuffle_idx]
 from sklearn.model_selection import train_test_split
 from sklearn import svm, metrics
 from sklearn.metrics import confusion_matrix
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-l, m, n = X_train.shape
-X_train = np.reshape(X_train, (l, m * n))
+from sklearn.preprocessing import StandardScaler
+X, X_test, y, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+l, m, n = X.shape
+X = np.reshape(X, (l, m * n))
 a, b, c = X_test.shape
 X_test = np.reshape(X_test, (a, b * c))
+print("printing the sizes:{},{}",X.shape,X_test.shape)
+scaler = StandardScaler()
+
+scaler.fit_transform(X)
+scaler.fit(X_test)
+
 
 clf = svm.SVC()
-clf.fit(X_train, y_train)
+clf.fit(X, y)
 y_pred = clf.predict(X_test)
 tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
 accuracy = (tp + tn) / (tp + tn + fp + fn)
