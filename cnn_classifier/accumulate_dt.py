3842d249b149cd8bb891efecb19ec28a67afcea1
sundarayamrita <sundarayamrita@gmail.com>
2021-02-12 18:58:09 +0530

    copied from covariance folder

---
+++ cnn_classifier/accumulate_dt.py
@@ -0,0 +1,111 @@
+import argparse
+from pathlib import Path
+from ac_correlation import auto_correlation, cross_correlation
+import numpy as np
+from physicochemical_distance import get_physico_dist
+
+# aaindex_path = Path.cwd() / "aaindex_format.txt"
+
+# parser = argparse.ArgumentParser()
+# parser.add_argument("pssm_dir", help = "pssm directory path", type = str)
+# parser.add_argument("seq_dir", help = "raw seq directory path", type = str)
+# parser.add_argument("--aaindex_dir", nargs = '?', default = aaindex_path, help = "aaindex1 directory path", type = str)
+# args = parser.parse_args()
+
+# if args.pssm_dir:
+#     pssm_dir = Path(args.pssm_dir)
+# if args.seq_dir:
+#     seq_dir = Path(args.seq_dir)
+# if args.aaindex_dir:
+#     aaindex_path = args.aaindex_dir
+
+# acc_all = []
+# for pssm_file in pssm_dir.iterdir():
+
+#     de1 = calc_de1(pssm_file)
+#     de2 = calc_de2(pssm_file)
+#     #acc = np.append(de2, de1, axis=1)
+#     acc = np.vstack((de2, de1.reshape((1, 20, 3))))
+#     acc_all.append(acc.reshape(-1, acc.shape[-1]))
+
+# acc_all = np.asarray(acc_all, dtype = np.float32)
+# print(acc_all.shape)
+
+
+# pdt_all = []
+# for seq_file in seq_dir.iterdir():
+#     pdt_all.append(get_physico_dist(seq_file, aaindex_path))
+
+# pdt_all = np.asarray(pdt_all, dtype = np.float32)
+# print(pdt_all.shape)
+
+# converted_dist = np.hstack((acc_all, pdt_all))
+# print(converted_dist.shape)
+# with open('neg.npy', 'wb') as f:
+#     np.save(f, converted_dist)
+
+def distance_transforms(pssm_dir, seq_dir):
+
+    alpha = 10
+    filename = str(seq_dir)
+    list_files = []
+    aaindex_path = Path.cwd() / "aaindex_format.txt"
+
+    acc_all = []
+    pssm_dir = Path(pssm_dir)
+    
+    for pssm_file in pssm_dir.iterdir():
+
+        list_files.append(pssm_file.stem)
+        de1 = auto_correlation(pssm_file)
+        de2 = cross_correlation(pssm_file)
+        
+        acc = np.hstack((de2, de1.reshape((20, 1, alpha))))
+        acc_all.append(acc.reshape(-1, acc.shape[-1]))
+
+    acc_all = np.asarray(acc_all, dtype = np.float32)
+
+    pdt_all = []
+    for seq_file in seq_dir.iterdir():
+        seq = seq_file.stem + "_pssm"
+        if seq in list_files:
+            pdt_all.append(get_physico_dist(seq_file, aaindex_path))
+
+    pdt_all = np.asarray(pdt_all, dtype = np.float32)
+
+    converted_dist = np.hstack((acc_all, pdt_all))
+    print("acc + pdt shape", converted_dist.shape)
+    with open((filename + '.npy'), 'wb') as f:
+            np.save(f, converted_dist)
+
+
+
+
+
+def standalone(seqs_dir, pssm_dir):
+
+    list_files = []
+    aaindex_path = Path.cwd() / "aaindex_format.txt"
+#     filename = "TRIALS"
+    for pssm_file in pssm_dir.iterdir():
+        distance_transforms(pssm_dir, seqs_dir)
+        list_files.append(pssm_file.stem)
+    
+    pdt_all = []
+    for seq_file in seqs_dir.iterdir():
+        seq = seq_file.stem + "_pssm"
+        if seq in list_files:
+            pdt_all.append(get_physico_dist(seq_file, aaindex_path))
+
+    pdt_all = np.asarray(pdt_all, dtype = np.float32)
+    print("standalone done", pdt_all.shape)
+#     distance_transforms(pssm_dir, seqs_dir) 
+
+    
+
+
+
+if __name__ == "__main__":
+   #standalone(Path(r"C:\Users\meera\OneDrive\Desktop\Homology\blast_gen_files_single\Remote-homology\Hybrid-SVM\pos-train.c.1.1"),Path(r"C:\Users\meera\OneDrive\Desktop\Homology\blast_gen_files_single\Remote-homology\Hybrid-SVM\pos-train.c.1.1PSSMs"))
+   #standalone(Path("/home/sundarayamrita/Documents/Programming/repos/remote_homology/acc_pdt_svm/indexed_files/c.1.1/pos-train.c.1.1"), Path("/home/sundarayamrita/Documents/Programming/repos/remote_homology/acc_pdt_svm/indexed_files/c.1.1/pos-train.c.1.1PSSMs"))
+   distance_transforms(Path("/home/sundarayamrita/Documents/Programming/repos/remote_homology/acc_pdt_svm/indexed_files/a.60.1/pos-test.a.60.1PSSMs"), Path("/home/sundarayamrita/Documents/Programming/repos/remote_homology/acc_pdt_svm/indexed_files/a.60.1/pos-test.a.60.1"))
---
+++ cnn_classifier/physicochemical_distance.py
@@ -0,0 +1,44 @@
+import numpy as np
+from pathlib import Path
+import argparse
+from normalise_aaindex import normalise  
+
+def get_physico_dist(file_loc, aa_index_path):
+    
+    with open(file_loc, "r") as f:
+        seq = f.readline()
+
+    L = len(seq)
+    alpha = 10
+
+    lookup = {
+            "A":1, "R":2, "N":3, "D":4, "C":5,
+            "Q":6, "E":7, "G":8, "H":9, "I":10,
+            "L":11, "K":12, "M":13, "F":14, "P":15,
+            "S":16, "T":17, "W":18, "Y":19, "V":20
+            }
+    
+    I = normalise(aa_index_path)
+    num_props = I.shape[0]
+    I = np.transpose(I)
+    #print("shape of I", I.shape)
+    PDT=[]
+
+    for mu in range(1, alpha + 1):
+
+        p1_idx = np.array([lookup.get(seq[i], 1) - 1 for i in range(L - mu)])
+        p2_idx = np.array([lookup.get(seq[i], 1) - 1 for i in range(mu , L)])
+        p1 = I[p1_idx]
+        p2 = I[p2_idx]
+        dp = np.sum(np.square(p1 - p2), axis = 0) / (L - mu)
+        #print("dp shape", dp.shape)
+        PDT.append(dp)
+
+    PDT = np.asarray(PDT).T
+    return PDT
+
+
+if __name__ == "__main__":
+    file_loc = Path.cwd() / "query_1.txt"
+    aa_index_path = Path.cwd() / "aaindex_format.txt"
+    get_physico_dist(file_loc, aa_index_path)
